efba317902bb1ab532aaae40896171dbf5a05291
christine-at-ruzuku <christine@ruzuku.com>
2016-06-10 12:05:52 -0400

    Uncommented out broken test which is now passing.

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -11,8 +11,8 @@ class SessionsHelperTest < ActionView::TestCase
     assert is_logged_in?
   end
 
-  # test "current_user returns nil when remember digest is wrong" do
-  #   @user.update_attribute(:remember_digest, User.digest(User.new_token))
-  #   assert_nil current_user
-  # end
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
 end
