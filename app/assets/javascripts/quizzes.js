77df68453da9b0f001e7d4cf010758875503f63a
christine-at-ruzuku <christine@ruzuku.com>
2016-07-11 11:01:10 -0400

    Updated add question button to add new field when clicked.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,16 +1,13 @@
 //= require jquery
 
 function remove_fields(link) {
-  console.log('remove fields called with:', link);
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
+  console.log('remove fields called with');
+  $(link).prev("input[type=hidden]").value = "1";
+  $(link).parent(".fields").hide();
 }
 
 function add_fields(link, association, content) {
-  console.log('add fields called with:', link, association, content);
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).parent().before(content.replace(regexp, new_id));
 }
