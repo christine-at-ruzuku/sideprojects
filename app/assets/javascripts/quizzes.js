fa22de0ac30447df5b12caebc27fb41aa1f858df
christine-at-ruzuku <christine@ruzuku.com>
2016-07-15 15:55:47 -0400

    Improved styling for answers.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,14 +1,13 @@
 //= require jquery
 
-function remove_fields(link) {
-  var removeButton = $(link).parent('div');
+function remove_field(link, type) {
+  var removeButton = type === 'simple'? $(link) : $(link).parent('div');
   var hiddenInput = removeButton.prev('input[type=hidden]');
-
   hiddenInput.val('true');
   removeButton.parent().hide();
 }
 
-function add_fields(link, association, content) {
+function add_field(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp('new_' + association, 'g');
   $(link).parent().before(content.replace(regexp, new_id));
--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,17 +1,22 @@
 module QuizzesHelper
 
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) +
-      "<div style=\"float:right;\"><span class=\"glyphicon glyphicon-remove\"></span> ".html_safe +
-      link_to(name, "javascript:void(0);", onclick: "remove_fields(this)") +
-      "</div>".html_safe
+  def link_to_remove_field(name, f, style)
+    if style == 'simple'
+      f.hidden_field(:_destroy) +
+        link_to('<span class="glyphicon glyphicon-remove"></span>'.html_safe, "javascript:void(0);", onclick: "remove_field(this, 'simple')", title: "Remove", alt: "Remove")
+    else
+      f.hidden_field(:_destroy) +
+        '<div class="right"><span class="glyphicon glyphicon-remove"></span> '.html_safe +
+        link_to(name, "javascript:void(0);", onclick: "remove_field(this)", title: "Remove", alt: "Remove") +
+        "</div>".html_safe
+    end
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_field(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to(name, "javascript:void(0);", onclick: "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    link_to(name, "javascript:void(0);", onclick: "add_field(this, \"#{association}\", \"#{escape_javascript(fields)}\")", title: "Add #{association}", alt: "Add #{association}")
   end
 end
